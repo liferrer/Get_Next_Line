fb52b34aad94f9177c0cc032c421aac2df4fc8b3
Lisa FERRERI <liferrer@e2r11p9.42.fr>
2020-03-03 14:36:11 +0100

    nouveau gnl, recursif, marche ap

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: liferrer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 14:51:29 by liferrer          #+#    #+#             */
-/*   Updated: 2020/03/02 17:11:12 by liferrer         ###   ########.fr       */
+/*   Updated: 2020/03/03 14:32:58 by liferrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,87 @@ int		get_next_line(int fd, char **line)
 {
 	int			i;
 	int			ret;
-	static char	*buffer[BUFF_SIZE];
+	char		*stock;
+	char		*tmp;
+	static char	*buffer[BUFFER_SIZE];
 
 	i = 0;
-	if (fd < 0 || !line || !(buffer = ft_strdup("")))
+	stock = NULL;
+	ret = 0;
+	if (fd < 0 || fd > OPEN_MAX || !line)
 		return (-1);
-	if (buffer[0] != '\0')
+	if (!stock)
+		stock = ft_strdup("");
+	if (stock)
 	{
-		if (ft_strchr(buffer, '\n'))
+		printf("boucle stock test\n");
+			printf("stock1 = %s \n", stock);
+		if (ft_strchr(stock, '\n'))
 		{
-			while (buffer[i] != '\n')
+		printf("boucle stock test 2\n");
+			while (stock[i] != '\n')
 				i++;
-			*line = ft_substr(buffer, 0, i + 1);
-			buffer = ft_substr(buffer, i + 1, ft_strlen(buffer + 1));
-			return (line);
+			*line = ft_substr(stock, 0, i);
+			printf("stock1 = %s \n", stock);
+			stock = ft_substr(stock, i + 1, ft_strlen(stock));
+			printf("stock2 = %s \n", stock);
+			return (1);
 		}
-		if (ret < BUFF_SIZE)
+		if (ret < BUFFER_SIZE)
 		{
-			*line = ft_strdup(buffer);
+			*line = ft_strdup(stock);
 			return (0);
 		}
 	}
-	while ((ret = read(fd, buffer, BUFF_SIZE) < 0))
+	while ((ret = read(fd, buffer, BUFFER_SIZE) > 0))
 	{
-		
+		if (!(ft_strchr((char*)buffer, '\n')))
+			*line = ft_strjoin(*line, (char*)buffer);
+		tmp = stock;
+		stock = ft_strjoin(stock, (char*)buffer);
+		free(tmp);
 	}
+	return (get_next_line(fd, line));
+}
+
+
+/*int		get_next_line(int fd, char **line)
+{
+	int			i;
+	int			ret;
+	static char	*buffer[BUFFER_SIZE];
+
+	i = 0;
+	ret = 0;
+	*line = NULL;
+	if (*line == NULL)
+		*line = ft_strdup("");
+	if (fd < 0 || fd > OPEN_MAX || !line)
+		return (-1);
+	while ((ret = read(fd, buffer, BUFFER_SIZE) > 0))
+	{
+		printf("buffer = %s\n", (char*)buffer);
+		*line = ft_strjoin(*line, (char*)buffer);
+	}
+	return (0);
+}*/
+
+int		main(void)
+{
+	int		fd;
+	int		ret;
+	char	*line;
+
+	fd = open("test.txt", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)))
+	{
+		if (ret == -1)
+		{
+			printf("error\n");
+			return(-1);
+		}
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("end ret: %d\n", ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,24 @@
 /*   By: liferrer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 14:53:18 by liferrer          #+#    #+#             */
-/*   Updated: 2020/03/02 14:53:50 by liferrer         ###   ########.fr       */
+/*   Updated: 2020/03/03 12:52:56 by liferrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(int fd, char **line);
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+//# define BUFFER_SIZE 42
+
+size_t		ft_strlen(const char *str);
+char		*ft_strchr(char *s, int c);
+char		*ft_strdup(char *s1);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+int			get_next_line(int fd, char **line);
+
+#endif
